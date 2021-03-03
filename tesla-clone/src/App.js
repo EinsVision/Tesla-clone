f3ad1725c2677ad62e5fa51fe7abd8809cf7a5e0
Changdae <einsvision@gmail.com>
2021-03-03 11:23:36 +0900

    building header

--- tesla-clone/src/App.js
+++ tesla-clone/src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import Header from './Header';
 
 function App() {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <Router>
       <div className="app">
-        <Header />
+        <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
         {/* HeaderBlock */}
       </div>
     </Router>
--- tesla-clone/src/Header.js
+++ tesla-clone/src/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Header.css';
 import { Link, Router } from 'react-router-dom';
+import CloseIcon from '@material-ui/icons/Close';
+import MenuIcon from '@material-ui/icons/Menu';
 
-function Header() {
+function Header({isMenuOpen, setIsMenuOpen}) {
   return (
     <div className='header'>
       <div className="header__logo">
@@ -13,6 +15,23 @@ function Header() {
             alt=""
           />
           </Link>
+      </div>
+
+      <div className="header__links">
+        <Link to='/'>Model S</Link>
+        <Link to='/'>Model 3</Link>
+        <Link to='/'>Model X</Link>
+        <Link to='/'>Model Y</Link>
+        <Link to='/'>CYBERTRUCK</Link>
+        <Link to='/'>POWERWALL</Link>
+      </div>
+
+      <div className="header__right">
+        <Link to=''>Shop</Link>
+        <Link to='/login'>Tesla Account</Link>
+        <div className="header__menu" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          {isMenuOpen ? <CloseIcon /> : <MenuIcon />}
+        </div>
       </div>
     </div>
   )
