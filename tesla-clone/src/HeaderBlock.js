fb4ff9a9a5f4ede2bd53d1268761d1ab877777de
Changdae <einsvision@gmail.com>
2021-03-03 13:30:14 +0900

    applied import Fade from 'react-reveal/Fade';

--- tesla-clone/src/HeaderBlock.js
+++ tesla-clone/src/HeaderBlock.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import './HeaderBlock.css';
+import Fade from 'react-reveal/Fade';
 
 function HeaderBlock() {
   return (
     <div className='headerBlock'>
       <div className="headerBlock__info">
         <div className="headerBlock__infoText">
-          <h1>Model 3</h1>
+          <Fade top> 
+            <h1>Model 3</h1>
+          </Fade>
+          
           <h4>
             Order Online for <span>Touchless Delivery</span>
           </h4>
         </div>
         <div className="headerBlock__actions">
-          <button className='headerBlock__buttonPrimary'>Custom order</button>
-          <button className='headerBlock__buttonSecondary'>Existing inventory</button>
+          <Fade left delay={300}>
+            <button className='headerBlock__buttonPrimary'>Custom order</button>
+          </Fade>
+          
+          <Fade right delay={300}>
+            <button className='headerBlock__buttonSecondary'>Existing inventory</button>
+          </Fade>
+          
         </div>
       </div>
     </div>
