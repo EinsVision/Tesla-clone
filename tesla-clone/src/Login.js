07ff5c509d732397225ec2e242892c9404bb49e4
Changdae <einsvision@gmail.com>
2021-03-03 16:50:04 +0900

    built login function using firebase auth

--- tesla-clone/src/Login.js
+++ tesla-clone/src/Login.js
@@ -1,16 +1,31 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import './Login.css';
 import LanguageIcon from '@material-ui/icons/Language';
 import ButtonPrimary from './ButtonPrimary';
 import ButtonSecondary from './ButtonSecondary';
+import { auth } from './firebase';
+import { useDispatch } from 'react-redux';
+import { login } from './features/userSlice';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   const signIn = (e) => {
     e.preventDefault();
+    auth.signInWithEmailAndPassword(email, password)
+    .then((userAuth) => {
+      dispatch(login({
+        email: userAuth.user.email,
+        uid: userAuth.user.uid,
+        displayName: userAuth.user.displayName,
+      }))
+      history.push('/teslaaccount');
+    })
+    .catch((error) => alert(error.message));
   }
 
   return (
