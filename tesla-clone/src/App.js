249df192d804dc994867fb7caa4bb14d3accfaee
Changdae <einsvision@gmail.com>
2021-03-03 16:09:13 +0900

    applied userSlice for distict user

--- tesla-clone/src/App.js
+++ tesla-clone/src/App.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
+import { selectUser } from './features/userSlice';
 import Header from './Header';
 import HeaderBlock from './HeaderBlock';
 import Login from './Login';
 import Menu from './Menu';
+import { useSelector } from 'react-redux';
 
 function App() {
-
+  const user = useSelector(selectUser);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
@@ -20,7 +22,8 @@ function App() {
             <HeaderBlock/>
           </Route>
           <Route path='/login'>
-            <Login />
+            {user ? <Redirect to='/teslaaccount'/> : <Login />}
+            
           </Route>  
         </Switch>        
       </div>
